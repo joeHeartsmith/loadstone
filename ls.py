09c6df0c648a2515ac31bd3ec9e10b7f38e96d79
Joe Jamison <joe@smaklab.com>
2023-08-09 22:13:12 -0500

    quick test

--- ls.py
+++ ls.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python3
 
-print("Hello, world")
+global lib_boto3 ; lib_boto3 = False
+
+"""
+try:
+   import boto3
+   lib_boto3 = True
+except:
+   print("IMPORT: no boto3")
+"""
+
+def aws_func(invar=None):
+   if lib_boto3 == False:
+       #raise Exception('No boto3')
+       print('no boto3')
+   else:
+        print("the aws version was called with " + str(invar) + ".")
+
+
+def azure_func(invar=None):
+   print("the azure version was called with " + str(invar) + ".")
+
+global aws ; aws = "aws"
+global azure ; azure = "azure"
+
+basic_func = {aws: aws_func, azure:azure_func}
+
+basic_func[aws]("TEST VALUE")
+basic_func[azure]("TEST VALUE")
+
+
