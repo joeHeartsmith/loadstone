77e3b880063522277d58368ca3da6cb5332b0f98
Joe Jamison <joe@smaklab.com>
2023-08-21 21:43:48 -0500

    update deps

--- azure_locallib.py
+++ azure_locallib.py
@@ -1,1 +1,15 @@
-# TODO: write azure functions here
+# Import the needed credential and management objects from the libraries.
+import os
+
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+
+# Acquire a credential object using CLI-based authentication.
+credential = AzureCliCredential()
+
+# Retrieve subscription ID from environment variable.
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
+
+# Obtain the management object for resources.
+resource_client = ResourceManagementClient(credential, subscription_id)
+
